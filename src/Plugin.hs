7aaa9deb518de53875f2966275887a2772df1c46
David Young <roboguy06@gmail.com>
2020-02-14 00:41:52 -0600

    Starting Core plugin

---
+++ src/Plugin.hs
@@ -0,0 +1,19 @@
+module Plugin (plugin) where
+
+import           Expr
+
+import           GhcPlugins
+
+plugin :: Plugin
+plugin =
+  defaultPlugin
+    { installCoreToDos = install
+    }
+
+install :: [CommandLineOption] -> [CoreToDo] -> CoreM [CoreToDo]
+install _ coreToDos = do
+  return (CoreDoPluginPass "Pattern matching plugin" pass : coreToDos)
+
+pass :: ModGuts -> CoreM ModGuts
+pass m = undefined
+
