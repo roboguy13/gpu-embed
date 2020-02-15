5e5d97564f2c7cb7ddb62103637f5368b8cb260e
David Young <roboguy06@gmail.com>
2020-02-14 19:59:03 -0600

    Work on pattern match transformation in plugin

--- src/Plugin.hs
+++ src/Plugin.hs
@@ -5,10 +5,10 @@
 
 module Plugin (plugin) where
 
-import           Expr hiding (Var, Lit, (:@))
+import           Expr hiding (Var, Lit, Lam, (:@))
 import qualified Expr
 
-import           Data.Data
+import           Data.Data hiding (TyCon (..), tyConName)
 import           Data.Generics.Uniplate.Operations
 import qualified Data.Generics.Uniplate.DataOnly as Data
 
@@ -16,38 +16,16 @@ import           GhcPlugins
 
 import           InstEnv
 
-import           TcRnTypes
-import           TcSimplify
-import           TcMType
-import           TcRnMonad
+import           FamInstEnv
+
 import           TcSMonad
-import           TcEvidence
-import           TcType
 
-import           DsBinds
-import           DsMonad
-
-import           HsBinds
-import           HsDecls
-
-import           CostCentreState
-
-import           Bag
-
-import           Encoding
-
-import           ErrUtils
-
-import           Data.Char
 
 import           Control.Monad
 
 import           Control.Arrow ((&&&), (***), first, second)
 
-import           Data.IORef
-
-import qualified Data.Map as Map
-import qualified Data.Set as Set
+import           GHCUtils
 
 -- Just for TH Names:
 import qualified Language.Haskell.TH.Syntax as TH
@@ -99,28 +77,36 @@ transformExpr guts primMap lookupVar = undefined
 -- | Transform primitives and constructor/function calls. Skips the
 -- function call transformation on the given 'recName' argument (if
 -- a 'Just').
-transformPrims :: ModGuts -> Maybe Var -> [(Id, Id)] -> (TH.Name -> Var) -> Expr Var -> Expr Var
-transformPrims guts recName primMap lookupVar = Data.transform go
+transformPrims :: ModGuts -> Maybe Var -> [(Id, Id)] -> (TH.Name -> Var) -> Expr Var -> CoreM (Expr Var)
+transformPrims guts recName primMap lookupVar = Data.transformM go
   where
     builtin :: Id -> Maybe (Expr Var)
     builtin v = Var <$> lookup v primMap
 
-    go expr@(Lit x) = Var (lookupVar 'Lit) :@ expr
+    -- Numeric literals
+    go expr@(Lit x :@ ty :@ dict) = return (Var (lookupVar 'Lit) :@ ty :@ dict :@ expr)
+
     go (Var v)
-      | Just b <- builtin v = b
+      | Just b <- builtin v = return b
 
-    go (Var f :@ x)
-      | Just b <- builtin f = b :@ x
+    go (Var f :@ tyA@(Type{}) :@ tyB@(Type{}) :@ dictA@(Type{}) :@ dictB@(Type{}) :@ x)
+      | Just b <- builtin f = return (b :@ tyA :@ tyB :@ dictA :@ dictB :@ x)
 
-    go (Var f :@ x :@ y)
-      | Just b <- builtin f = b :@ x :@ y
+    go (Var f :@ ty@(Type{}) :@ dict@(Type{}) :@ x)
+      | Just b <- builtin f = return (b :@ ty :@ dict :@ x)
+
+    go (Var f :@ ty@(Type{}) :@ dict@(Type{}) :@ x :@ y)
+      | Just b <- builtin f = return (b :@ ty :@ dict :@ x :@ y)
 
     go expr@(Var v)
-      | v `elem` map lookupVar ['(,), '(,,), '(,,,)] =
-          Var (lookupVar 'construct) :@ expr
+      | v `elem` map lookupVar ['(,), '(,,), '(,,,)] = do
+          let vType = varType v
+          tyCon <- findTyCon' guts (varName (lookupVar ''ConstructC))
+
+          return (Var (lookupVar 'construct) :@ Type vType :@ Type (mkTyConApp tyCon [vType]) :@ expr)
 
     go expr@(Var fn :@ _)
-      | fn == lookupVar 'construct = expr
+      | fn == lookupVar 'construct = return $ expr
 
     go expr@(Case e wild ty alts@((altCon1, _, _):_))
       | DataAlt d <- altCon1 =
@@ -130,11 +116,57 @@ transformPrims guts recName primMap lookupVar = Data.transform go
                 Just falseBody = lookup (getName (lookupVar 'False)) iteAlts
                 Just trueBody = lookup (getName (lookupVar 'True)) iteAlts
             in
-            Var (lookupVar 'IfThenElse) :@ e :@ trueBody :@ falseBody
+            return (Var (lookupVar 'IfThenElse) :@ Type ty :@ e :@ trueBody :@ falseBody)
           else
-            expr
+            return expr
 
-    go expr = expr
+    go expr = return expr
+
+transformProdMatch :: ModGuts -> (TH.Name -> Var) -> Type -> Alt a -> CoreM (Expr a)
+transformProdMatch guts lookupVar ty0 (altCon@(DataAlt dataCon), vars0, body) = do
+  repTyCon <- findTyCon' guts (varName (lookupVar ''GPURep))
+
+  repType <- computeRepType guts (dataConOrigResTy dataCon)
+
+  prodTypes <- listTypesWith (lookupVar ''(,)) repType
+
+  pairTyCon <- findTyCon' guts (varName (lookupVar ''(,)))
+
+  return $ go pairTyCon repTyCon prodTypes vars0
+  where
+    go pairTyCon repTyCon (ty1:_) [] = Var (lookupVar 'NullaryMatch) :@ Type ty1 :@ Type ty0 :@ Type (mkTyConApp repTyCon [ty0]) :@ body
+    go pairTyCon repTyCon (ty1:_) [x] =
+      Var (lookupVar 'OneProdMatch) :@ Type ty1 :@ Type ty0 :@ Type (mkTyConApp repTyCon [ty0]) :@ body
+
+    go pairTyCon repTyCon (ty1:restTys) (x:xs) =
+      let restTy = (mkTyConApp pairTyCon restTys)
+      in
+      Var (lookupVar 'ProdMatch)
+        :@ Type ty1
+        :@ Type restTy
+        :@ Type ty0
+        :@ Type (mkTyConApp repTyCon [ty1])
+        :@ Type (mkTyConApp repTyCon [restTy])
+        :@ go pairTyCon repTyCon restTys xs
+
+transformSumMatch :: ModGuts -> (TH.Name -> Var) -> Expr b -> b -> Type -> [Alt b] -> CoreM (Expr a)
+transformSumMatch guts lookupVar scrutinee wild ty alts@(alt1:_) = do
+  undefined
+
+abstractOver :: a -> Expr a -> Expr a
+abstractOver = Lam
+
+-- | listTypesWith (lookupVar ''(,)) (a, (b, c))  ==>  [a, b, c]
+-- listTypesWith (lookupVar ''Either) (Either a (Either b c))  ==>  [a, b, c]
+listTypesWith :: Var -> Type -> CoreM [Type]
+listTypesWith tyConVar ty =
+  case splitTyConApp_maybe ty of
+    Nothing -> return [ty]
+    Just (tyCon, [a, b])
+      | tyConName tyCon /= getName tyConVar -> return [ty]
+      | otherwise ->
+          fmap (a:) (listTypesWith tyConVar b)
+
 
 thNameToGhcName_ :: TH.Name -> CoreM Name
 thNameToGhcName_ thName = do
@@ -149,7 +181,7 @@ primMapTH =
   ,('fromEnum, 'FromEnum)
   ,('fromIntegral, 'FromIntegral)
   ,('sqrt, 'Sqrt)
-  ,('the, 'the_repr)
+  -- ,('the, 'the_repr)
 
   ,('False, 'FalseExp)
   ,('True, 'TrueExp)
@@ -227,262 +259,14 @@ constrNamesTH =
   ]
 
 
--- Slightly modified from HERMIT:
--- | Build a dictionary for the given
-buildDictionary :: ModGuts -> Id -> CoreM (Id, [CoreBind])
-buildDictionary guts evar = do
-    (i, bs) <- runTcM guts $ do
-#if __GLASGOW_HASKELL__ > 710 
-        loc <- getCtLocM (GivenOrigin UnkSkol) Nothing
-#else
-        loc <- getCtLoc $ GivenOrigin UnkSkol
-#endif
-        let predTy = varType evar
-#if __GLASGOW_HASKELL__ > 710 
-            nonC = mkNonCanonical $ CtWanted { ctev_pred = predTy, ctev_dest = EvVarDest evar, ctev_loc = loc }
-            wCs = mkSimpleWC [cc_ev nonC]
-        -- TODO: Make sure solveWanteds is the right function to call.
-        (_wCs', bnds) <- second evBindMapBinds <$> runTcS (solveWanteds wCs)
-#else
-            nonC = mkNonCanonical $ CtWanted { ctev_pred = predTy, ctev_evar = evar, ctev_loc = loc }
-            wCs = mkSimpleWC [nonC]
-        (_wCs', bnds) <- solveWantedsTcM wCs
-#endif
-        -- reportAllUnsolved _wCs' -- this is causing a panic with dictionary instantiation
-                                  -- revist and fix!
-        return (evar, bnds)
-    bnds <- runDsM guts $ dsEvBinds bs
-    return (i,bnds)
+-- | Use GPURepTy to find the canonical representation type
+computeRepType :: ModGuts -> Type -> CoreM Type
+computeRepType guts ty = do
+  gpuRepTy <- thNameToGhcName_ ''GPURepTy
 
-buildDictionaryT :: ModGuts -> Type -> CoreM CoreExpr
-buildDictionaryT guts = \ ty -> do
-    dflags <- getDynFlags
-    binder <- newIdH ("$d" ++ zEncodeString (filter (not . isSpace) (showPpr dflags ty))) ty
-    (i,bnds) <- buildDictionary guts binder
-    when (notNull bnds) (error "no dictionary bindings generated.")
-    -- guardMsg (notNull bnds) "no dictionary bindings generated."
-    return $ case bnds of
-                [NonRec v e] | i == v -> e -- the common case that we would have gotten a single non-recursive let
-                _ -> mkCoreLets bnds (varToCoreExpr i)
+  repTy <- findTyCon' guts gpuRepTy
 
-
-runTcM :: ModGuts -> TcM a -> CoreM a
-runTcM guts m = do
-    env <- getHscEnv
-    dflags <- getDynFlags
-    -- What is the effect of HsSrcFile (should we be using something else?)
-    -- What should the boolean flag be set to?
-    (msgs, mr) <- liftIO $ initTcFromModGuts env guts HsSrcFile False m
-    let showMsgs (warns, errs) = showSDoc dflags $ vcat
-                                                 $    text "Errors:" : pprErrMsgBagWithLoc errs
-                                                   ++ text "Warnings:" : pprErrMsgBagWithLoc warns
-    maybe (error $ showMsgs msgs) return mr
-
-runDsM :: ModGuts -> DsM a -> CoreM a
-runDsM guts = runTcM guts . initDsTc
-
--- | Make a unique identifier for a specified type, using a provided name.
-newIdH :: MonadUnique m => String -> Type -> m Id
-newIdH nm ty = mkLocalId <$> newName' nm <*> return ty
-
--- | Make a 'Name' from a string.
-newName' :: MonadUnique m => String -> m Name
-newName' nm = mkSystemVarName <$> getUniqueM <*> return (mkFastString nm)
-
--- | Re-Setup the typechecking environment from a ModGuts
-initTcFromModGuts
-    :: HscEnv
-    -> ModGuts
-    -> HscSource
-    -> Bool          -- True <=> retain renamed syntax trees
-    -> TcM r
-    -> IO (Messages, Maybe r) -- Nothing => error thrown by the thing inside
-                              -- (error messages should have been printed already)
-initTcFromModGuts hsc_env guts hsc_src keep_rn_syntax do_this
- = do { let { type_env = mk_type_env guts } ;
-        errs_var     <- newIORef (emptyBag, emptyBag) ;
-        tvs_var      <- newIORef emptyVarSet ;
-        keep_var     <- newIORef emptyNameSet ;
-#if __GLASGOW_HASKELL__ > 710 
-        used_gre_var <- newIORef [] ;
-#else
-        used_rdr_var <- newIORef Set.empty ;
-#endif
-        th_var       <- newIORef False ;
-        th_splice_var<- newIORef False ;
-#if __GLASGOW_HASKELL__ > 710 
-        infer_var    <- newIORef (True, emptyBag) ;
-#else
-        infer_var    <- newIORef True ;
-#endif
-        lie_var      <- newIORef emptyWC ;
-        dfun_n_var   <- newIORef (mk_dfun_n guts) ;
-        type_env_var <- newIORef type_env ;
-
-        dependent_files_var <- newIORef [] ;
-        static_wc_var       <- newIORef emptyWC ;
-
-        th_topdecls_var      <- newIORef [] ;
-        th_topnames_var      <- newIORef emptyNameSet ;
-        th_modfinalizers_var <- newIORef [] ;
-        th_state_var         <- newIORef Map.empty ;
-#if __GLASGOW_HASKELL__ > 710 
-        th_remote_state_var  <- newIORef Nothing ;
-#endif
-
-
-        th_top_level_locs_var <- newIORef (Set.fromList []) ;
-        th_foreign_files_var <- newIORef [] ;
-        th_coreplugins_var <- newIORef [] ;
-
-{- TODO: Verify implementations are correct for:
-           tcg_semantic_mod,
-           tcg_th_top_level_locs, tcg_merged, tcg_th_foreign_files,
-           tcg_th_coreplugins, tcg_top_loc, tcg_complete_matches,
-           tcg_cc_st
- -}
-
-        costCentreState_var <- newIORef newCostCentreState ;
-
-        let {
-             dflags = hsc_dflags hsc_env ;
-             mod = mg_module guts ;
-             realSrcLoc = realSrcLocSpan $ mkRealSrcLoc (fsLit "Top level") 1 1 ;
-
-             maybe_rn_syntax :: forall a. a -> Maybe a ;
-             maybe_rn_syntax empty_val
-                | keep_rn_syntax = Just empty_val
-                | otherwise      = Nothing ;
-
-             gbl_env = TcGblEnv {
-                tcg_semantic_mod = mod, -- TODO: Is this ok?
-
-                tcg_th_top_level_locs = th_top_level_locs_var,
-                tcg_merged = [],
-                tcg_th_foreign_files = th_foreign_files_var,
-                tcg_th_coreplugins = th_coreplugins_var,
-                tcg_top_loc = realSrcLoc,
-                tcg_complete_matches = [],
-                tcg_cc_st = costCentreState_var,
-
-                -- these first four are CPP'd in GHC itself, but we include them here
-                tcg_th_topdecls      = th_topdecls_var,
-                tcg_th_topnames      = th_topnames_var,
-                tcg_th_modfinalizers = th_modfinalizers_var,
-                tcg_th_state         = th_state_var,
-
-                -- queried during tcrnif
-                tcg_mod            = mod,
-                tcg_src            = hsc_src,
-                -- tcg_sig_of         = getSigOf dflags (moduleName mod),
-                -- tcg_impl_rdr_env   = Nothing,
-                tcg_rdr_env        = mg_rdr_env guts,
-                tcg_default        = Nothing,
-                tcg_fix_env        = mg_fix_env guts,
-                tcg_field_env      = mk_field_env guts,
-                tcg_type_env       = type_env,
-                tcg_type_env_var   = type_env_var,
-                tcg_inst_env       = mg_inst_env guts,
-                tcg_fam_inst_env   = mg_fam_inst_env guts,
-                tcg_ann_env        = emptyAnnEnv,
-#if __GLASGOW_HASKELL__ <= 710 
-                tcg_visible_orphan_mods = mkModuleSet [mod],
-#endif
-                tcg_dfun_n         = dfun_n_var,
-
-                -- accumulated, not queried, during tcrnif
-                tcg_dependent_files = dependent_files_var,
-                tcg_tc_plugins     = [],
-                tcg_static_wc      = static_wc_var,
-                tcg_exports        = [],
-                tcg_warns          = NoWarnings,
-                tcg_anns           = [],
-                tcg_tcs            = [],
-                tcg_insts          = [],
-                tcg_fam_insts      = [],
-                tcg_rules          = [],
-                tcg_th_used        = th_var,
-                tcg_imports        = emptyImportAvails,
-                tcg_dus            = emptyDUs,
-                tcg_ev_binds       = emptyBag,
-                tcg_fords          = [],
-                -- tcg_vects          = [],
-                tcg_patsyns        = [],
-                tcg_doc_hdr        = Nothing,
-                tcg_hpc            = False,
-                tcg_main           = Nothing,
-                tcg_safeInfer      = infer_var,
-                tcg_binds          = emptyLHsBinds,
-                tcg_sigs           = emptyNameSet,
-                tcg_imp_specs      = [],
-                tcg_rn_decls       = maybe_rn_syntax emptyRnGroup,
-#if __GLASGOW_HASKELL__ <= 710 
-                tcg_used_rdrnames  = used_rdr_var,
-#endif
-                tcg_rn_imports     = [],
-                tcg_rn_exports     = maybe_rn_syntax [],
-                tcg_keep           = keep_var,
-#if __GLASGOW_HASKELL__ > 710 
-                tcg_self_boot      = NoSelfBoot, -- Assume there are no hsboot files
-                tcg_used_gres       = used_gre_var,
-                tcg_th_remote_state = th_remote_state_var,
-                tcg_tr_module       = Nothing,
-#endif
-                tcg_th_splice_used = th_splice_var
-             } ;
-             lcl_env = TcLclEnv {
-                tcl_errs       = errs_var,
-                tcl_loc        = realSrcLoc,
-                tcl_ctxt       = [],
-                tcl_rdr        = emptyLocalRdrEnv,
-                tcl_th_ctxt    = topStage,
-                tcl_th_bndrs   = emptyNameEnv,
-                tcl_arrow_ctxt = NoArrowCtxt,
-                tcl_env        = emptyNameEnv,
-                tcl_bndrs      = [],
-                -- tcl_tidy       = emptyTidyEnv,
-                tcl_tyvars     = tvs_var,
-                tcl_lie        = lie_var,
-                tcl_tclvl      = topTcLevel
-             } ;
-        } ;
-
-        -- OK, here's the business end!
-        maybe_res <- initTcRnIf 'a' hsc_env gbl_env lcl_env $
-                     do { r <- tryM do_this
-                        ; case r of
-                          Right res -> return (Just res)
-                          Left _    -> return Nothing } ;
-
-        -- Check for unsolved constraints
-        lie <- readIORef lie_var ;
-        if isEmptyWC lie
-           then return ()
-           else pprPanic "initTc: unsolved constraints" (ppr lie) ;
-
-        -- Collect any error messages
-        msgs <- readIORef errs_var ;
-
-        let { final_res | errorsFound dflags msgs = Nothing
-                        | otherwise               = maybe_res } ;
-
-        return (msgs, final_res)
-    }
-
-mk_type_env :: ModGuts -> TypeEnv
--- copied from GHC.compileCore
-mk_type_env guts = typeEnvFromEntities (bindersOfBinds (mg_binds guts))
-                                           (mg_tcs guts)
-                                           (mg_fam_insts guts)
-mk_field_env :: ModGuts -> RecFieldEnv
--- TODO
-#if __GLASGOW_HASKELL__ > 710
-mk_field_env _ = emptyNameEnv 
-#else
-mk_field_env _ = RecFields emptyNameEnv emptyNameSet
-#endif
-
-mk_dfun_n :: ModGuts -> OccSet
--- TODO
-mk_dfun_n _ = emptyOccSet
+  runTcM guts . fmap fst . runTcS $ do
+    famInstEnvs <- getFamInstEnvs
+    return (topNormaliseType famInstEnvs (mkTyConApp repTy [ty]))
 
